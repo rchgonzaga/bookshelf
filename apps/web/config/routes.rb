09cdaec1b95b3464ebb45b0459c99013c24d9547
rchgonzaga <rchgonzaga@gmail.com>
2020-02-28 00:12:53 -0300

    feature: added add books route and form

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -5,4 +5,7 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 root to: 'home#index'
 # get '/', to: 'home#index'
+get '/books/new', to: 'books#new'
 get '/books', to: 'books#index'
+post '/books', to: 'books#create'
+get '/books/new', to: 'books#new'
